12260cbe9b8b544786fe3d8d760a87176433f6fd
1k-ct <59008019+1k-ct@users.noreply.github.com>
2020-05-13 03:39:50 +0900

    Update get_youtubelive_comment.py

--- get_youtubelive_comment.py
+++ get_youtubelive_comment.py
@@ -10,26 +10,25 @@ def fuuso(target_urls, absolute_path):
         get_live_comment3.get_comment(target_url, absolute_path)
 
 def main():
-    #
+    
     target_urls = ['https://www.youtube.com/watch?v=ljA6mKiTQuc',
                    'https://www.youtube.com/watch?v=VVQdAdUdowE',
                    ]
-    # ファイルを保存する場所
+    # ファイルを保存する場所("絶対パス")
     absolute_path = "D:\comments"
 
+    # 紫咲シオンという人のチャンネルの動画urlを全て取得
+    # coe = coeuter.Coeuter()
+    # channel_url = 'https://www.youtube.com/channel/UCXTpFs_3PqI41qX2d9tL2Rw'# 紫咲シオン ch.
+    # x = 55000
+    # print(coe.mninj(channel_url,x))# 戻り値は(list)
     
-    coe = coeuter.Coeuter()
-    channel_url = 'https://www.youtube.com/channel/UCXTpFs_3PqI41qX2d9tL2Rw'
-    x = 55000
-    print(coe.mninj(channel_url,x))# 戻り値は(list)
-
+    # youtube live コメントを全て、取得
     # fuuso(target_urls, absolute_path)
 
-    # グラフ表示したいtarget_urlをしたに入れる
-    # read_json_comment.show_graph(target_url, file_name, absolute_path)
-    target_url = 'https://www.youtube.com/watch?v=ljA6mKiTQuc'
+    # グラフ表示したいtarget_urlを代入
+    # target_url = 'https://www.youtube.com/watch?v=ljA6mKiTQuc'
     # read_json_comment.show_graph(target_url, absolute_path)
 
 if __name__ == "__main__":
     main()
-    #pip install pyautogui
